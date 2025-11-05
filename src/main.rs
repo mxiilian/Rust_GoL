bc1e4f81845e602f8d390e5af3205c6755bd0d9b
m.kotsarev <maximilian-vadim.kotsarev@stud.hs-hannover.de>
2025-11-05 16:49:35 +0100

    ppause machen

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+extern crate glutin_window;
+extern crate graphics;
+extern crate opengl_graphics;
+extern crate piston;
+
+use glutin_window::GlutinWindow as Window;
+use opengl_graphics::{GlGraphics, OpenGL};
+use piston::event_loop::{EventSettings, Events};
+use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
+use piston::window::WindowSettings;
+
+pub struct App {
+    // Art Klasse
+    gl: GlGraphics, //opengl drawing backend
+    rotation: f64,  // für die rotation
+}
+
+impl App {
+    //hier kommen die Funktionen für die Klasse rein
+    fn render(&mut self, args: &RenderArgs) {
+        use graphics::*;
+
+        const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 0.0];
+        const RED: [f32; 4] = [1.0, 0.0, 0.0, 0.0];
+    }
 }
